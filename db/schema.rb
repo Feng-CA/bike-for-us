ff769dd3bfe3f068dd686c639d3ad43639fbbeeb
Feng-CA <gcam022106@coderacademy.edu.au>
2022-03-04 23:55:39 +1100

    sets up Type, Size, Gender and State models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_054354) do
+ActiveRecord::Schema.define(version: 2022_03_04_125120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genders", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sizes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+types = ["Hybrid", "Mountain", "Cruiser", "BMX", "Urban", "Road", "Fixed Gear", "Electric"]
+
+if Type.count == 0 
+  types.each do |t| 
+    Type.create(name: t)
+    puts "created #{t} type"
+  end
+end
+
+sizes = ['<16"', '16"', '18"', '20"', '24"', '26"', '27.5"', '>27.5"']
+
+if Size.count == 0 
+  sizes.each do |s| 
+    Size.create(name: s)
+    puts "created #{s} size"
+  end
+end
+
+genders = ["Mens", "Womens", "Unisex"]
+
+if Gender.count == 0 
+  genders.each do |g| 
+    Gender.create(name: g)
+    puts "created #{g} gender"
+  end
+end
+
+states = ["ACT", "NSW", "NT", "QLD", "SA", "VIC", "TAS", "WA"]
+
+if State.count == 0 
+  states.each do |s| 
+    State.create(name: s)
+    puts "created #{s} state"
+  end
+end
