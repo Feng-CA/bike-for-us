3fd3ce23d8523901433965c239896cb19e09d3c4
Feng-CA <gcam022106@coderacademy.edu.au>
2022-03-05 17:07:03 +1100

    adds create and update functionalities

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,10 @@
 class ListingsController < ApplicationController
+  # Sets up user access authentication except index and show page
   before_action :authenticate_user!, except: [:index, :show]
+  # refectors listing instance before show, edit, update and destory
   before_action :set_listing, only: [:show, :edit, :update, :destory]
+  # This before action helps new and edit views
+  before_action :set_form_vars, only: [:new, :edit]
   def index
     @listings = Listing.all
   end
@@ -14,7 +18,13 @@ class ListingsController < ApplicationController
   end
 
   def create
-  
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing successfully created"
+    else
+      set_form_vars
+      render "new", notice: "Something went wrong!"
+    end 
   end
 
   def edit
@@ -22,7 +32,13 @@ class ListingsController < ApplicationController
   end
 
   def update
-  
+    @listing.update(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing successfully updated"
+    else
+      set_form_vars
+      render "edit", notice: "Something went wrong"
+    end 
   end
 
   def destory
@@ -31,9 +47,20 @@ class ListingsController < ApplicationController
 
   private
 
+  def listing_params
+    params.require(:listing).permit(:title, :price, :type_id, :size_id, :gender_id, :suburb, :state_id, :description)
+  end
+
   def set_listing
     @listing = Listing.find(params[:id])
   end
+
+  def set_form_vars
+    @types = Type.all
+    @sizes = Size.all
+    @genders = Gender.all
+    @states = State.all
+  end 
 
 
 end
