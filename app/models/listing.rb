48dbb8573ce58905b5528222e7b8aeaf49620a02
Feng-CA <gcam022106@coderacademy.edu.au>
2022-03-06 14:03:38 +1100

    sets up data validation into Listing model and alert in listing view

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,5 +7,11 @@ class Listing < ApplicationRecord
   has_many :listings_features, dependent: :destroy
   has_many :features, through: :listings_features
 
+  # allows one picture upload into listing
   has_one_attached :picture
+
+  # validations - allows valide data save into database
+  validates :title, :description, :price, :type, :size, :gender, :suburb, :state, presence: true 
+  validates :title, length: {minimum: 4, maximum: 20}
+  validates :description, length: {minimum: 4, maximum: 500}
 end
