2f662250e90887f1b320f37157785231077bdd09
Feng-CA <gcam022106@coderacademy.edu.au>
2022-03-08 22:43:02 +1100

    sets up staging environment on heroku

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module BikeForUs
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+     # add support for custom environments in heroku
+     if ENV["PIPE_ENV"].present?
+      Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/#{ENV["PIPE_ENV"]}.yml.enc")
+    end
   end
 end
